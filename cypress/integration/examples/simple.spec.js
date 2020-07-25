45213e470e02627add7125000f33c9de1bacfe38
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-07-25 11:26:04 -0500

    example spec

---
+++ cypress/integration/examples/simple.spec.js
@@ -0,0 +1,15 @@
+describe('My first simple test', () => {
+	it('True should be true', () => {
+		expect(true).to.equal(true)
+	})
+
+	it('5 should be 5', () => {
+		expect(5).to.equal(5)
+	})
+})
+
+describe('Another describe block', () => {
+	it('False should be false', () => {
+		expect(false).to.equal(false)
+	})
+})
